0cda324c9f923a75c19b8ee6736f53f158af168b
Binh Le <lebinh190998@gmail.com>
2023-10-27 21:46:16 +0700

    remove poetry

--- server/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-poetry lock
-poetry config virtualenvs.create false
-
-poetry install --no-interaction --no-ansi --no-dev
--- server/scripts/entry_point.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-# gunicorn breed_detection.asgi:app -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
-python run.py
